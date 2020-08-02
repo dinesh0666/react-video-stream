57b9dbb45e304bf4361407d220ee6d6b7d6ca266
dinesh0666 <dineshkumar06@yahoo.com>
2020-08-02 12:19:14 +0530

    live stream addedd

--- index.js
+++ index.js
@@ -7,17 +7,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      videoSrc: null
     };
+  }
+
+  ComponentDidMount(){
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true}, this.handleVideo, this.videoError);
+    }
+  }
+
+  handleVideo =(stream) => {
+    // Update the state, triggering the component to re-render with the correct stream
+    this.setState({ videoSrc: window.URL.createObjectURL(stream) });
+  }
+
+  videoError = () => {
+    alert('WebCAM NOT WORKING');
   }
 
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+          <video src={this.state.videoSrc} autoPlay={true} />
       </div>
     );
   }
