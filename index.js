d2288e4548107e7d050655edf719bd99da28e228
dinesh0666 <dineshkumar06@yahoo.com>
2020-08-02 15:06:24 +0530

    video

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+const videoType = 'video/webm';
 
-class App extends Component {
-  constructor() {
-    super();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      videoSrc: null
+      recording: false,
+      videos: [],
     };
   }
 
-  ComponentDidMount(){
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true}, this.handleVideo, this.videoError);
-    }
+  async componentDidMount() {
+    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: { echoCancellation: false }});
+    this.video.srcObject = stream;
+    this.video.play();
+    this.mediaRecorder = new MediaRecorder(stream);
+    this.chunks = [];
+    this.mediaRecorder.ondataavailable = e => {
+      if (e.data && e.data.size > 0) {
+        this.chunks.push(e.data);
+      }
+    };
   }
 
-  handleVideo =(stream) => {
-    // Update the state, triggering the component to re-render with the correct stream
-    this.setState({ videoSrc: window.URL.createObjectURL(stream) });
+  startRecording(e) {
+    e.preventDefault();
+    this.chunks = [];
+    this.mediaRecorder.start(10);
+    this.setState({recording: true});
   }
 
-  videoError = () => {
-    alert('WebCAM NOT WORKING');
+  stopRecording(e) {
+    e.preventDefault();
+    this.mediaRecorder.stop();
+    this.setState({recording: false});
+    this.saveVideo();
+  }
+
+  saveVideo() {
+    const blob = new Blob(this.chunks, {type: videoType});
+    const videoURL = window.URL.createObjectURL(blob);
+    const videos = this.state.videos.concat([videoURL]);
+    console.log({videos})
+    this.setState({videos});
+  }
+
+  deleteVideo(videoURL) {
+    const videos = this.state.videos.filter(a => a !== videoURL);
+    this.setState({videos});
   }
 
   render() {
+    const {recording, videos} = this.state;
     return (
-      <div>
-          <video src={this.state.videoSrc} autoPlay={true} />
+      <div className="camera">
+        <video
+          style={{width: 400}}
+          ref={a => {
+            this.video = a;
+          }}>
+         <p>Video stream not available. </p>
+        </video>
+        <div>
+          {!recording && <button onClick={e => this.startRecording(e)}>Start Record</button>}
+          {recording && <button onClick={e => this.stopRecording(e)}>Stop</button>}
+        </div>
+        <div>
+          <h3>Recorded Videos:</h3>
+          {videos.length === 0 &&
+              <h3>No videos available</h3>
+          }
+          {videos.map((videoURL, i) => (
+            <div key={`video_${i}`}>
+              <video controls style={{width: 200}} src={videoURL}   />
+              <div>
+                <button onClick={() => this.deleteVideo(videoURL)}>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
+export default App
 
 render(<App />, document.getElementById('root'));
